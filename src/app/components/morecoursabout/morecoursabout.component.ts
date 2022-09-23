76b61e35b1f8ebf567cbd3479d7771aa04b23b8c
rafaelyeghoyan <rvvebcss@gmail.com>
2022-09-23 19:08:58 +0400

    adding final response on the page morecoursabout

--- src/app/components/morecoursabout/morecoursabout.component.ts
+++ src/app/components/morecoursabout/morecoursabout.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-morecoursabout',
@@ -7,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MorecoursaboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private el:ElementRef) { }
 
   ngOnInit(): void {
     window.scrollTo(0,0)
+  }
+  openDropDown() {
+    this.el.nativeElement.querySelector('header .dropdown').style.width = "250px";
+    this.el.nativeElement.querySelector('header .dropdown').style.height = "100vh";
+    this.el.nativeElement.querySelector('header .dropdown').style.overflow = "visible";
+    this.el.nativeElement.querySelector('header .dropdown').style.transition = "height 0.8s";
+  }
+  closeDropDown() {
+    this.el.nativeElement.querySelector('header .dropdown').style.width = "0";
+    this.el.nativeElement.querySelector('header .dropdown').style.width = "0";
+    this.el.nativeElement.querySelector('header .dropdown').style.overflow = "hidden";
+    this.el.nativeElement.querySelector('header .dropdown').style.transition = "height 0.8s";
   }
 
 }
